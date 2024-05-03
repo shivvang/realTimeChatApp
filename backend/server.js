04df67d201108574172506cebe2c6bbe936404ec
shivvang <shivanghariakar@email.com>
2024-05-03 22:32:01 +0530

    i forgot to implement socket ,still not finished ,will learn this stuff instead of copy pasting stuff

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { connectionToMongoDb } from "./db/connectionToDb.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //to parse incoming requests
@@ -21,7 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectionToMongoDb();
   console.log(`the server is up and running in the port ${PORT}`);
 });
